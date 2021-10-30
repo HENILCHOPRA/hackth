2c71e914fc4e5b2bb25af2ac3fff30dd63a4c311
HENIL M CHOPADA <chopdahenil@gmail.com>
2021-10-30 16:44:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,13 @@
-from flask import Flask, render_template, request, session, redirect, url_for, Response, jsonify, send_from_directory
+from flask import Flask, render_template, request
+import pickle
+app = Flask(__name__)
+# Loading the Pre-trained model using Pickle
+with open('./LogisticRegression.pkl', 'rb') as file:
+    model = pickle.load(file)
 
-application = Flask(__name__)
-
-@application.route('/')
-def index():
+@app.route('/', methods=['GET', 'POST'])
+def main():
     return jsonify({'message': 'Success',}), 200
 
-@application.route('/register', methods=['POST', 'GET'])
-def register():
-    if 'user_name' and 'role' in session and session['role'] == 'admin':
-        stxid = request.form['contact']
-        length = request.form['marital']
-        width = request.form['duration']
-        height = request.form['age']
-        dept = request.form['education']
-        dept = request.form['job']
-        dept = request.form['month']
-        return jsonify({'message': 'Success',}), 200
-    else:
-        return jsonify({'message': 'Error',}), 500
-
 if __name__ == '__main__':
-    application.run(debug=False)
-    
+    app.run()
