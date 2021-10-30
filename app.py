470c45f7090dff08abe1db9a32c68c9fd03223a9
HENIL M CHOPADA <chopdahenil@gmail.com>
2021-10-30 16:18:35 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, session, redirect, url_for, Response, jsonify, send_from_directory
+
+application = Flask(__name__)
+
+@application.route('/register', methods=['POST', 'GET'])
+def register():
+    if 'user_name' and 'role' in session and session['role'] == 'admin':
+        stxid = request.form['contact']
+        length = request.form['marital']
+        width = request.form['duration']
+        height = request.form['age']
+        dept = request.form['education']
+        dept = request.form['job']
+        dept = request.form['month']
+        return jsonify({'message': 'Success',}), 200
+    else:
+        return jsonify({'message': 'Error',}), 500
+
+if __name__ == '__main__':
+    application.run(host='155.33.134.47', port=8080, threaded=True, debug=False)
+    
